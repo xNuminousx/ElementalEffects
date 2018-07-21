8df43e4712e34ad84ca2ff0bdce10a456e483250
Daddy Pride <xixipepperd@gmail.com>
2018-07-21 05:54:46 -0400

    Update Move.java

--- src/com/xnuminousx/elementaleffects/trails/Move.java
+++ src/com/xnuminousx/elementaleffects/trails/Move.java
@@ -50,6 +50,7 @@ public class Move implements Listener {
 		new BukkitRunnable() {
 			double t = 0;
 			Main plugin = Main.getInstance();
+			int amount = Main.getInstance().getConfig().getInt("Trails.Lava.Particles.Amount");
 			public void run() {
 				t += Math.PI / 16;
 				Location loc = p.getLocation();
@@ -59,7 +60,7 @@ public class Move implements Listener {
 					double z = 0.3 * (4 * Math.PI - t) * Math.sin(t + phi);
 					loc.add(x, y, z);
 					GeneralMethods.displayColoredParticle(loc, "f47142", 0F, 0F, 0F);
-					ParticleEffect.DRIP_LAVA.display(loc, 0F, 0F, 0F, 0F, 3);
+					ParticleEffect.BLOCK_CRACK.display(new BlockData(Material.LAVA, (byte) 0), 0F, 0F, 0F, 0F, amount, loc, 257D);
 					loc.subtract(x, y, z);
 				
 					if (t >= 4 * Math.PI) {
